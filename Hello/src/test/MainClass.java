094d1588017ae41f470e28c58d0fe4154ae6c8ed
violin977@naver.com <violin977@naver.com>
2020-11-18 12:14:20 +0900

    Hello 프로젝트 추가

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,11 @@
+package test;
+// 빨간색들은 예약어라고 하는데 어떤경우에도 변수로 사용할 수 없다.
+// MainClass 라는 이름의 클래스
+public class MainClass {
+		// main 이라는 이름의 메소드(함수)
+		public static void main(String[] args) {
+				// <script> </script> 와 { } 가 같은 의미라고 보면됨.
+				System.out.println("Hello, World!");
+				
+		}
+}
